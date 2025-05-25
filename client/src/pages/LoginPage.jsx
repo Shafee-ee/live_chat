1e56879b37d6037ee45e43c057c1d3a4a8785f2c
shafee_azeem <shafeeghani@gmail.com>
2025-05-25 07:05:51 +0530

    test 1

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
 
     return (
         <div className='min-h-screen bg-cover bg-center 
-        flex items-center justofy-center gap-8 
+        flex items-center justify-center gap-8 
         sm:justify-evenly max-sm:flex-col backdrop-blur-2xl '>
             {/* left side login*/}
             <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
